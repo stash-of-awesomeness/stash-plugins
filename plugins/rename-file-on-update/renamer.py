9d0eacbd76fab76d600b23f7e7911a325ec04d08
stash-of-awesomeness <194728131+stash-of-awesomeness@users.noreply.github.com>
2025-07-21 13:18:24 -0400

    Pull partial scene fragment
    This is needed to combat incompatibilities in the latest GraphQL
    schema for Stash.

--- plugins/rename-file-on-update/renamer.py
+++ plugins/rename-file-on-update/renamer.py
@@ -3,19 +3,48 @@ from stashapi import log
 from config_parser import Config
 from file_manager import StashFile
 
+SCENE_FRAGMENT = """
+id
+title
+code
+date
+organized
+
+studio {
+    id
+    name
+    parent_studio {
+        id
+        name
+    }
+}
+
+files {
+    id
+    path
+    basename
+
+    format
+    width
+    height
+    duration
+    video_codec
+    audio_codec
+}
+"""
 
 def rename_scene(stash: StashInterface, config: Config, args):
     log.info(f"Checking scene with args: {args}")
 
     scene_id = args["hookContext"]["id"]
-    scene = stash.find_scene(scene_id)
+    scene = stash.find_scene(scene_id, fragment=SCENE_FRAGMENT)
 
     if not config.rename_unorganized and not scene["organized"]:
         log.info("Scene is not marked as organized, ignoring scene.")
         return
 
     for file in scene["files"]:
-        stash_file = StashFile(config, scene, file)
+        stash_file = StashFile(stash, config, scene, file)
 
         old_path = stash_file.get_old_file_path()
         new_path = stash_file.get_new_file_path()
